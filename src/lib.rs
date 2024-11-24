ecd73d35c62ebb754392305393eaf17c0b872139
Maxim Volkov <37216852+Handy-caT@users.noreply.github.com>
2024-11-24 20:27:21 +0300

    Add `parse_data_page` function (#8)
    - move `parse_info` and `parse_index` to general function

--- src/lib.rs
+++ src/lib.rs
@@ -9,9 +9,9 @@ pub use link::Link;
 pub use data_bucket_codegen::{PersistIndex, PersistTable, SizeMeasure};
 pub use page::{
     map_data_pages_to_general, map_index_pages_to_general, map_tree_index, map_unique_tree_index,
-    parse_index, parse_info, persist_page, Data as DataPage, General as GeneralPage, GeneralHeader,
-    IndexPage as IndexData, Interval, PageType, SpaceInfo as SpaceInfoData, HEADER_SIZE,
-    INNER_PAGE_SIZE, PAGE_SIZE,
+    parse_data_page, parse_page, persist_page, Data as DataPage, General as GeneralPage,
+    GeneralHeader, IndexPage as IndexData, Interval, PageType, SpaceInfo as SpaceInfoData,
+    HEADER_SIZE, INNER_PAGE_SIZE, PAGE_SIZE,
 };
 pub use persistence::{PersistableIndex, PersistableTable};
 pub use util::{align, Persistable, SizeMeasurable};
--- src/page/mod.rs
+++ src/page/mod.rs
@@ -14,7 +14,8 @@ pub use index::{map_tree_index, map_unique_tree_index, IndexPage};
 pub use space_info::{Interval, SpaceInfo};
 pub use ty::PageType;
 pub use util::{
-    map_data_pages_to_general, map_index_pages_to_general, parse_index, parse_info, persist_page,
+    map_data_pages_to_general, map_index_pages_to_general, parse_data_page, parse_page,
+    persist_page,
 };
 
 // TODO: Move to config
