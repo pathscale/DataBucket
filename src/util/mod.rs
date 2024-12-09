e710c5c30f82b8d886f87b6f8b70a09916014f58
Andrew Tsibin <andrewtsibin@gmail.com>
2024-12-09 22:08:34 +0300

    Add measurable wrapper for index

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,5 +1,7 @@
 mod persistable;
 mod sized;
+mod tree_index;
 
 pub use persistable::Persistable;
 pub use sized::{align, SizeMeasurable};
+pub use tree_index::{MeasuredTreeIndex, MeasuredMultiTreeIndex};
