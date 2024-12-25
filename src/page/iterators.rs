5db7cb2dba477c207fcbd23a8572f624bbc280b6
Alexander Rodin <rodin.alexander@gmail.com>
2024-12-25 15:20:47 +0300

    Add `DataIterator` and a test for it

--- src/page/iterators.rs
+++ src/page/iterators.rs
@@ -2,9 +2,9 @@ use std::io::Read;
 
 use rkyv::{api::high::HighDeserializer, Archive};
 
-use crate::{page::util::parse_general_header, IndexData, Link};
+use crate::{page::util::parse_general_header, persistence::data::{rkyv_data::parse_archived_row, DataTypeValue}, IndexData, Link};
 
-use super::{index::IndexValue, seek_to_page_start, Interval};
+use super::{index::IndexValue, seek_by_link, seek_to_page_start, Interval};
 
 pub struct PageIterator {
     intervals: Vec<Interval>,
@@ -122,9 +122,51 @@ where
     }
 }
 
+struct DataIterator<'a> {
+    file: &'a mut std::fs::File,
+    schema: Vec<(String, String)>,
+    links: Vec<Link>,
+    link_index: usize,
+}
+
+impl DataIterator<'_> {
+    pub fn new(file: &mut std::fs::File, schema: Vec<(String, String)>, mut links: Vec<Link>) -> DataIterator<'_> {
+        links.sort_by(|a, b| (a.page_id, a.offset).partial_cmp(&(b.page_id, b.offset)).unwrap());
+
+        DataIterator {
+            file,
+            schema,
+            links,
+            link_index: 0,
+        }
+    }
+}
+
+impl Iterator for DataIterator<'_> {
+    type Item = Vec<DataTypeValue>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.link_index >= self.links.len() {
+            return None;
+        }
+
+        let current_link = self.links[self.link_index];
+        seek_by_link(&mut self.file, current_link).expect("the seek should be successful");
+        let mut buffer = vec![0u8; current_link.length as usize];
+        self.file.read_exact(&mut buffer).expect("the data should be read");
+        let row = parse_archived_row(&buffer, &self.schema);
+
+        self.link_index += 1;
+
+        Some(row)
+    }
+}
+
 #[cfg(test)]
 mod test {
-    use crate::{page::PageId, Interval, Link};
+    use crate::{
+        page::{self, iterators::DataIterator, util::parse_space_info, PageId}, persistence::data::DataTypeValue, Interval, Link, PAGE_SIZE
+    };
 
     use super::{LinksIterator, PageIterator};
 
@@ -146,7 +188,7 @@ mod test {
 
     #[test]
     fn test_links_iterator() {
-        let filename = "tests/data/table_with_rows.wt";
+        let filename = "tests/data/table_links_test.wt";
         super::super::util::test::create_test_database_file(filename);
 
         let mut file = std::fs::File::open(filename).unwrap();
@@ -166,5 +208,25 @@ mod test {
                 }
             ]
         );
+    }
+
+    #[test]
+    fn test_pages_and_links_iterators() {
+        let filename = "tests/data/table_pages_and_links_test.wt";
+        super::super::util::test::create_test_database_file(filename);
+
+        let mut file = std::fs::File::open(filename).unwrap();
+        let space_info = parse_space_info::<PAGE_SIZE>(&mut file).unwrap();
+        let index_intervals = space_info.primary_key_intervals;
+
+        let pages_ids = PageIterator::new(index_intervals).collect::<Vec<_>>();
+        assert_eq!(pages_ids, vec![1]);
+
+        let links = LinksIterator::<'_, i32>::new(&mut file, pages_ids[0]).collect::<Vec<_>>();
+        let data_iterator: DataIterator<'_> = DataIterator::new(&mut file, space_info.row_schema, links);
+        assert_eq!(data_iterator.collect::<Vec<_>>(), vec![
+            vec![DataTypeValue::I32(1), DataTypeValue::String("first string".to_string())],
+            vec![DataTypeValue::I32(2), DataTypeValue::String("second string".to_string())]
+        ]);
     }
 }
