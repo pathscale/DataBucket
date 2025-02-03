fd17a97069d14c3ed7ffe15bb153a717c7c745ad
Handy-caT <37216852+Handy-caT@users.noreply.github.com>
2025-02-03 03:21:18 +0300

    corrections for multiset

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ pub use link::Link;
 
 pub use data_bucket_codegen::SizeMeasure;
 pub use page::{
-    map_data_pages_to_general, map_index_pages_to_general, map_tree_index, map_unique_tree_index,
+    map_data_pages_to_general, map_index_pages_to_general, map_tree_index,
     parse_data_page, parse_index_page, parse_page, persist_page, read_index_pages, seek_by_link,
     seek_to_page_start, update_at, Data as DataPage, DataType, General as GeneralPage,
     GeneralHeader, IndexPage as IndexData, Interval, PageType, SpaceInfo as SpaceInfoData,
